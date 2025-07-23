480df17b1d0f9c34fc69af4a90a87ff56b26742d
Egenue <gene23.chiki@gmail.com>
2025-07-23 09:00:25 +0300

    New backend link

--- server/app.js
+++ server/app.js
@@ -34,7 +34,7 @@ mongoose.connect(process.env.MONGO_URL, {
 
 const corsOptions = {
   origin: [ 'http://localhost:3000',
-    'https://eduwise-d120.onrender.com',
+    'https://eduwise-2.onrender.com',
     'https://eduwise-1-s0ex.onrender.com' ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
@@ -50,5 +50,5 @@ app.options('*', cors(corsOptions));
 //   res.status(500).json({ error: 'Internal server error' });
 // });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
